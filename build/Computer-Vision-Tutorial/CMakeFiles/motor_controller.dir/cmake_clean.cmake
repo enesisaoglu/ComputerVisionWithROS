3efcb0811b6afc97e87aadabe203a0539e9ef67c
enesisaoglu <enesisaogluu@gmail.com>
2023-09-13 21:10:16 +0300

    Basic stuff and Submodule

---
+++ build/Computer-Vision-Tutorial/CMakeFiles/motor_controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/enesisaoglu/robot2_ws/devel/lib/computer_vision_pkg/motor_controller"
+  "/home/enesisaoglu/robot2_ws/devel/lib/computer_vision_pkg/motor_controller.pdb"
+  "CMakeFiles/motor_controller.dir/src/motor_controller.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/motor_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
